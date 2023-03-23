dae3cc5448879553a6eba2a2755ac95d3e609184
mahabradai <br.maha.coder@gmail.com>
2023-03-23 14:47:02 +0100

    first commit

--- src/app/fruits/fruits-routing.module.ts
+++ src/app/fruits/fruits-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
 
 const routes: Routes = [
   {
     path: 'fruits/home',
     component: HomeComponent,
+  },{
+    path: 'fruits/create',
+    component: CreateComponent,
+
   },
+  {
+    path:'fruits/edit/:id',
+    component: EditComponent
+  }
 ];
 
 @NgModule({
